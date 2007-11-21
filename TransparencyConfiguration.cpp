c55fb0324d556c672416f931c967a18e2f62d853
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2007-11-21 19:56:20 +0000

    Transparency global configuration

---
+++ TransparencyConfiguration.cpp
@@ -0,0 +1,138 @@
+// $Id$
+
+/******************************************************************************
+*                         
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>             
+*                         
+* This is free software; you can redistribute it and/or modify it under the    
+* terms of the GNU General Public License as published by the Free Software    
+* Foundation; either version 2 of the License, or (at your option) any later   
+* version.                             
+*                          
+* This software is distributed in the hope that it will be useful, but WITHOUT 
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License        
+* for more details.                     
+*                          
+* You should have received a copy of the GNU General Public License along with 
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple     
+* Place, Suite 330, Boston, MA  02111-1307 USA                           
+*                         
+*                         
+*******************************************************************************/
+
+/*!
+   \file    TransparencyConfiguration.cpp
+   \brief   transparency common configuration
+   \version $Revision$
+   \date    $Date$
+*/
+
+#include <QLabel>
+#include <QLayout>
+
+#include "CustomGridLayout.h"
+#include "Debug.h"
+#include "OptionCheckBox.h"
+#include "OptionColorDisplay.h"
+#include "OptionSlider.h"
+#include "OptionSpinBox.h"
+#include "TransparencyConfiguration.h"
+
+using namespace std;
+using namespace TRANSPARENCY;
+
+//________________________________________________________________________
+TransparencyConfiguration::TransparencyConfiguration( QWidget* parent ):
+  QGroupBox( "Transparency", parent ),
+  Counter( "TransparencyConfiguration" )
+{
+  Debug::Throw( "TransparencyConfiguration::TransparencyConfiguration.\n" );
+  
+  
+  QVBoxLayout *layout( new QVBoxLayout() );
+  layout->setSpacing(5);
+  layout->setMargin(5);
+  setLayout( layout );
+   
+
+  // generic objects
+  OptionColorDisplay* color_display;
+  OptionSlider* slider;
+  OptionSpinBox* spinbox;
+  OptionCheckBox* checkbox;
+  
+  // enable/disable transparency
+  layout->addWidget( checkbox = new OptionCheckBox( "use transparency", this, "TRANSPARENT" ) );
+  checkbox->setToolTip( "enable/disable transparent background" );
+  addOptionWidget( checkbox );
+    
+  CustomGridLayout* grid_layout = new CustomGridLayout();
+  grid_layout->setSpacing(5);
+  grid_layout->setMargin(0);
+  grid_layout->setMaxCount( 2 );
+  layout->addLayout( grid_layout );
+
+  // foreground color
+  grid_layout->addWidget( new QLabel( "foreground color:", this ) );
+  grid_layout->addWidget( color_display = new OptionColorDisplay( this, "FOREGROUND_COLOR" ) );
+  color_display->setToolTip( "text/display foreground color" );
+  addOptionWidget( color_display );
+ 
+  // shadow color
+  grid_layout->addWidget( new QLabel( "shadow color:", this ) );
+  grid_layout->addWidget( color_display = new OptionColorDisplay( this, "SHADOW_COLOR" ) );
+  color_display->setToolTip( "text/display shadow color" );
+  addOptionWidget( color_display );
+ 
+  // shadow offset
+  grid_layout->addWidget( new QLabel( "shadow offset: ", this ) );
+  grid_layout->addWidget( spinbox = new OptionSpinBox( this, "SHADOW_OFFSET" ) );
+  spinbox->setMinimum( 0 );
+  spinbox->setMaximum( 10 );
+  spinbox->setToolTip( 
+    "offset between text shadow and text.\n"
+    "0 means no shadow." );
+  addOptionWidget( spinbox );
+ 
+  // tint color
+  grid_layout->addWidget( new QLabel( "tint color:", this ) );
+  grid_layout->addWidget( color_display = new OptionColorDisplay( this, "TINT_COLOR" ) );
+  color_display->setToolTip( "transparent background tint color" );
+  addOptionWidget( color_display );
+  
+  // tint intensity
+  grid_layout->addWidget( new QLabel( "tint intensity:", this ) );
+  grid_layout->addWidget( slider = new OptionSlider( this, "TINT_INTENSITY" ) );
+  slider->setScale( 1.0/2.55 );
+  slider->slider().setMinimum( 0 );
+  slider->slider().setMaximum( 100 );
+  slider->setToolTip( "transparent background tint intensity" );
+  addOptionWidget( slider );
+
+  // highlight color
+  grid_layout->addWidget( new QLabel( "highlight color:", this ) );
+  grid_layout->addWidget( color_display = new OptionColorDisplay( this, "HIGHLIGHT_COLOR" ) );
+  color_display->setToolTip( "highlight color when entering widget" );
+  addOptionWidget( color_display );
+  
+  // tint intensity
+  grid_layout->addWidget( new QLabel( "highlight intensity:", this ) );
+  grid_layout->addWidget( slider = new OptionSlider( this, "HIGHLIGHT_INTENSITY" ) );
+  slider->setScale( 1.0/2.55 );
+  slider->slider().setMinimum( 0 );
+  slider->slider().setMaximum( 100 );
+  slider->setToolTip( "transparent background highlight intensity" );
+  addOptionWidget( slider );
+
+  // opacity
+  grid_layout->addWidget( new QLabel( "opacity (xcomposite):", this ) );
+  grid_layout->addWidget( slider = new OptionSlider( this, "OPACITY" ) );
+  slider->setScale( 1.0/2.55 );
+  slider->slider().setMinimum( 0 );
+  slider->slider().setMaximum( 100 );
+  slider->setToolTip( "widget opacity" );
+  addOptionWidget( slider );
+
+  return;
+}
---
+++ TransparencyConfiguration.h
@@ -0,0 +1,51 @@
+#ifndef _TransparencyConfiguration_h_
+#define _TransparencyConfiguration_h_
+
+// $Id$
+
+/******************************************************************************
+*                         
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>             
+*                         
+* This is free software; you can redistribute it and/or modify it under the    
+* terms of the GNU General Public License as published by the Free Software    
+* Foundation; either version 2 of the License, or (at your option) any later   
+* version.                             
+*                          
+* This software is distributed in the hope that it will be useful, but WITHOUT 
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License        
+* for more details.                     
+*                          
+* You should have received a copy of the GNU General Public License along with 
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple     
+* Place, Suite 330, Boston, MA  02111-1307 USA                           
+*                         
+*                         
+*******************************************************************************/
+
+/*!
+   \file    TransparencyConfiguration.h
+   \brief   transparency common configuration
+   \version $Revision$
+   \date    $Date$
+*/
+
+#include <QGroupBox>
+#include "Counter.h"
+#include "OptionWidgetList.h"
+
+namespace TRANSPARENCY
+{
+  //! transparency common configuration
+  class TransparencyConfiguration: public QGroupBox, public OptionWidgetList, public Counter
+  {
+    public:
+    
+    //! constructor
+    TransparencyConfiguration( QWidget* parent );
+    
+  };
+};
+
+#endif
