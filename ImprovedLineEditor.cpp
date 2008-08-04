5e1868b875e0ffcd9c590d72eab24021c5318762
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2008-08-04 22:57:24 +0000

    *** empty log message ***

---
+++ ImprovedLineEditor.cpp
@@ -0,0 +1,135 @@
+// $Id$
+
+/******************************************************************************
+*                         
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>             
+*                         
+* This is free software; you can redistribute it and/or modify it under the    
+* terms of the GNU General Public License as published by the Free Software    
+* Foundation; either version 2 of the License, or (at your option) any later   
+* version.                             
+*                          
+* This software is distributed in the hope that it will be useful, but WITHOUT 
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License        
+* for more details.                     
+*                          
+* You should have received a copy of the GNU General Public License along with 
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple     
+* Place, Suite 330, Boston, MA  02111-1307 USA                           
+*                         
+*                         
+*******************************************************************************/
+ 
+/*!
+  \file ImprovedLineEditor.cpp
+  \brief customized QLineEdit object
+  \author Hugo Pereira
+  \version $Revision$
+  \date $Date$
+*/
+
+#include <QApplication>
+#include <QLayout>
+#include <QPainter>
+#include <QStyle>
+#include <QStyleOptionFrameV2>
+
+#include "BaseIcons.h"
+#include "Debug.h"
+#include "IconEngine.h"
+#include "ImprovedLineEditor.h"
+#include "QtUtil.h"
+
+using namespace std;
+
+//______________________________________________________________
+ImprovedLineEditor::ImprovedLineEditor( QWidget* parent ):
+  QFrame( parent ),
+  Counter( "ImprovedLineEditor" )
+{
+  Debug::Throw( "ImprovedLineEditor::ImprovedLineEditor.\n" );
+  
+  setFrameStyle( QFrame::StyledPanel|QFrame::Sunken );
+  QHBoxLayout* layout = new QHBoxLayout();
+  layout->setMargin(0);
+  layout->setSpacing(0);
+  setLayout( layout );
+  
+  // editor
+  layout->addWidget( editor_ = new LineEditor( this ), 1 );
+  editor_->setFrame( false );
+  
+  // clear button
+  layout->addWidget( clear_button_ = new Button( this ), 0 );
+  clear_button_->setIcon( IconEngine::get( ICONS::CLEAR ) );
+  int width = editor().fontMetrics().lineSpacing();
+  clear_button_->setFixedSize( width + 1, width );
+  
+  // connections
+  connect( &editor(), SIGNAL( returnPressed() ), SIGNAL( returnPressed() ) );
+  connect( &editor(), SIGNAL( cursorPositionChanged( int ) ), SIGNAL( cursorPositionChanged( int ) ) );
+  connect( &editor(), SIGNAL( modificationChanged( bool ) ), SIGNAL( modificationChanged( bool ) ) );
+  connect( &editor(), SIGNAL( textChanged( const QString& ) ), SLOT( _textChanged( const QString& ) ) );
+  connect( &editor(), SIGNAL( textChanged( const QString& ) ), SIGNAL( textChanged( const QString& ) ) );
+  connect( qApp, SIGNAL( focusChanged( QWidget*, QWidget* ) ), SLOT( update() ) );
+
+  connect( clear_button_, SIGNAL( clicked() ), &editor(), SLOT( clear() ) );
+  
+  // size
+  QtUtil::fixSize( this, QtUtil::HEIGHT );
+  
+  // hide button by default since text is empty
+  _clearButton().hide();
+}
+
+//________________________________________________
+void ImprovedLineEditor::paintEvent( QPaintEvent* event )
+{
+  
+  Debug::Throw( "ImprovedLineEditor::paintEvent.\n" );
+
+  // this is a hack from QFrame::paintEvent
+  // to handle focus.  
+  QPainter painter(this);
+  painter.save();
+  painter.setPen( Qt::NoPen );
+  painter.setBrush( editor().palette().color( QPalette::Base ) );
+  QRect rect( frameRect() );
+  int offset( frameWidth() );
+  rect.adjust( offset, offset, -offset, -offset );
+  painter.drawRect( rect );
+  painter.restore();
+  
+  QStyleOptionFrameV2 panel;
+  panel.initFrom( this );
+  panel.rect = frameRect();
+  panel.state |= QStyle::State_Sunken;
+  if( editor().hasFocus() ) panel.state |= QStyle::State_HasFocus;
+  style()->drawPrimitive(QStyle::PE_Frame, &panel, &painter, this);
+  painter.end();
+  
+}
+
+//________________________________________________
+void ImprovedLineEditor::_textChanged( const QString& text )
+{
+  if( text.isEmpty() )
+  {
+    if( _clearButton().isVisible() ) _clearButton().hide();
+    return;
+  } else if( !_clearButton().isVisible() ) _clearButton().show();
+  return;
+}
+
+//________________________________________________
+void ImprovedLineEditor::Button::paintEvent( QPaintEvent* event )
+{
+  
+  QPainter painter( this );
+  QRect rect( this->rect() );
+  rect.adjust( 0, -1, 0, 1 );
+  icon().paint( &painter, rect );
+  painter.end();
+  
+}
---
+++ ImprovedLineEditor.h
@@ -0,0 +1,169 @@
+#ifndef _ImprovedLineEditor_h_
+#define _ImprovedLineEditor_h_
+
+// $Id$
+
+/******************************************************************************
+*                         
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>             
+*                         
+* This is free software; you can redistribute it and/or modify it under the    
+* terms of the GNU General Public License as published by the Free Software    
+* Foundation; either version 2 of the License, or (at your option) any later   
+* version.                             
+*                          
+* This software is distributed in the hope that it will be useful, but WITHOUT 
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License        
+* for more details.                     
+*                          
+* You should have received a copy of the GNU General Public License along with 
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple     
+* Place, Suite 330, Boston, MA  02111-1307 USA                           
+*                         
+*                         
+*******************************************************************************/
+ 
+/*!
+  \file ImprovedLineEditor.h
+  \brief customized QLineEdit object
+  \author Hugo Pereira
+  \version $Revision$
+  \date $Date$
+*/
+
+#include <assert.h>
+#include <QFrame>
+#include <QString>
+#include <QPaintEvent>
+#include <QAbstractButton>
+
+#include "Counter.h"
+#include "LineEditor.h"
+
+/*!
+   \class ImprovedLineEditor
+   \brief customized line edit to have faster popup menu
+*/
+class ImprovedLineEditor: public QFrame, public Counter
+{
+
+  //! Qt meta object declaration
+  Q_OBJECT
+
+  public:
+  
+  //! constructor;
+  ImprovedLineEditor( QWidget* parent );
+
+  LineEditor& editor( void ) const
+  { 
+    assert( editor_ );
+    return *editor_;
+  }
+
+  QString text( void ) const
+  { return editor().text(); }
+
+  //! modification
+  bool isModified( void ) const
+  { return editor().isModified(); }
+
+  //! history
+  bool isUndoAvailable( void ) const
+  { return editor().isUndoAvailable(); }  
+
+  //! history
+  bool isRedoAvailable( void ) const
+  { return editor().isRedoAvailable(); }  
+  
+  //! read-only
+  void setReadOnly( const bool& value ) const
+  { editor().setReadOnly( value ); }
+  
+  //! set modified
+  void setModified( const bool& value ) const
+  { editor().setModified( value ); }
+  
+  //! cursor position
+  void setCursorPosition( int value ) const
+  { editor().setCursorPosition( value ); }
+  
+  signals:
+  
+  //! return pressed
+  void returnPressed();
+
+  //! text changed
+  void textChanged( const QString& );
+  
+  //! signal emmited when the cursor changes position
+  void cursorPositionChanged( int position );  
+  
+  //! modification state changed
+  void modificationChanged( bool );
+  
+  public slots:
+  
+  //! select all
+  void selectAll( void )
+  { editor().selectAll(); }
+  
+  //! set text
+  void setText( const QString& value )
+  { editor().setText( value ); }
+  
+  //! undo
+  void undo( void )
+  { editor().undo(); }
+  
+  //! undo
+  void redo( void )
+  { editor().redo(); }
+
+  protected:
+  
+  //! paint
+  void paintEvent( QPaintEvent* );
+  
+  //! button
+  class Button: public QAbstractButton
+  {
+    
+    public: 
+    
+    //! constructor
+    Button( QWidget* parent ):
+      QAbstractButton( parent )
+      {}
+    
+    protected:
+    
+    //! paint
+    void paintEvent( QPaintEvent* );
+    
+  };
+  
+  // clear button
+  Button& _clearButton() const
+  { 
+    assert( clear_button_ );
+    return *clear_button_;
+  }
+  
+  protected slots:
+  
+  //! show hide clear button depending on text length
+  void _textChanged( const QString& );
+  
+  private:
+  
+  //! editor
+  LineEditor* editor_;
+  
+  //! clear button
+  Button* clear_button_;
+  
+};
+
+#endif
