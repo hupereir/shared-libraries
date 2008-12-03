cb88d664a97b77b26131d8a508fea8adebdda2ff
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2008-12-03 04:40:36 +0000

    move 'add' and 'cancel-button' icons to base-pixmaps

--- Options.cpp
+++ Options.cpp
@@ -1,3 +1,4 @@
+
 // $Id$
 
 /******************************************************************************
@@ -216,7 +217,7 @@ std::ostream &operator << (std::ostream &out,const Options &options)
 //________________________________________________
 Options::Map::const_iterator Options::_find( const std::string& name ) const
 {
-  // cout << "Options::_find - Looking for option " << name << endl;
+  //cout << "Options::_find - Looking for option " << name << endl;
   Map::const_iterator out( options_.find( name ) );
   assert( out != options_.end() );
   return out;
