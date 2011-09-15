5560fca8b928e62864677477a5d5a40f15630458
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2011-09-15 08:21:33 +0000

    implementing session save/restore

--- XmlOptions.cpp
+++ XmlOptions.cpp
@@ -1,33 +1,33 @@
 // $Id$
 
 /******************************************************************************
- *
- * Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
- *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA  02111-1307 USA
- *
- *
- *******************************************************************************/
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple
+* Place, Suite 330, Boston, MA  02111-1307 USA
+*
+*
+*******************************************************************************/
 
 
 /*!
-  \file XmlOptions.cpp
-  \brief Option file parser based on xml
-  \author Hugo Pereira
-  \version $Revision$
-  \date $Date$
+\file XmlOptions.cpp
+\brief Option file parser based on xml
+\author Hugo Pereira
+\version $Revision$
+\date $Date$
 */
 
 #include <QDomDocument>
@@ -45,91 +45,91 @@ using namespace std;
 //____________________________________________________________________
 QString& XmlOptions::file( void )
 {
-  static QString file;
-  return file;
+    static QString file;
+    return file;
 }
 
 //____________________________________________________________________
 XmlError& XmlOptions::error( void )
 {
-  static XmlError error;
-  return error;
+    static XmlError error;
+    return error;
 }
 
 //____________________________________________________________________
 Options& XmlOptions::get( void )
 {
-  static Options singleton( true );
-  return singleton;
+    static Options singleton( true );
+    return singleton;
 }
 
 //____________________________________________________________________
 bool XmlOptions::read( QString file )
 {
 
-  Debug::Throw() << "XmlOptions::read - file=\"" << file << "\"\n";
+    Debug::Throw() << "XmlOptions::read - file=\"" << file << "\"\n";
 
-  // check filename is valid
-  if( file.isEmpty() ) file = XmlOptions::file();
-  if( file.isEmpty() ) return false;
+    // check filename is valid
+    if( file.isEmpty() ) file = XmlOptions::file();
+    if( file.isEmpty() ) return false;
 
-  // store requested file
-  XmlOptions::file() = file;
+    // store requested file
+    XmlOptions::file() = file;
 
-  // parse the file
-  QFile qtfile( file );
-  if ( !qtfile.open( QIODevice::ReadOnly ) )
-  {
-    Debug::Throw( "XmlOptions::read - cannot open file.\n" );
-    return false;
-  }
-
-  // dom document
-  QDomDocument document;
-  error() = XmlError( file );
-  if ( !document.setContent( &qtfile, &error().error(), &error().line(), &error().column() ) )
-  {
-    qtfile.close();
-    return false;
-  }
-
-  QDomElement doc_element = document.documentElement();
-  QDomNode node = doc_element.firstChild();
-  for(QDomNode node = doc_element.firstChild(); !node.isNull(); node = node.nextSibling() )
-  {
-    QDomElement element = node.toElement();
-    if( element.isNull() ) continue;
-
-    // special options
-    if( element.tagName() == OPTIONS::SPECIAL_OPTION )
+    // parse the file
+    QFile qtfile( file );
+    if ( !qtfile.open( QIODevice::ReadOnly ) )
     {
+        Debug::Throw( "XmlOptions::read - cannot open file.\n" );
+        return false;
+    }
 
-      Debug::Throw( "XmlOptions::read - special options" );
+    // dom document
+    QDomDocument document;
+    error() = XmlError( file );
+    if ( !document.setContent( &qtfile, &error().error(), &error().line(), &error().column() ) )
+    {
+        qtfile.close();
+        return false;
+    }
 
-      // retrieve Value attribute
-      QString value( element.attribute( OPTIONS::VALUE ) );
-      if( value.size() ) get().keep( value );
+    QDomElement doc_element = document.documentElement();
+    QDomNode node = doc_element.firstChild();
+    for(QDomNode node = doc_element.firstChild(); !node.isNull(); node = node.nextSibling() )
+    {
+        QDomElement element = node.toElement();
+        if( element.isNull() ) continue;
 
-    } else if( element.tagName() == OPTIONS::OPTION ) {
+        // special options
+        if( element.tagName() == OPTIONS::SPECIAL_OPTION )
+        {
 
-      XmlOption option( element );
-      if( get().isSpecialOption( option.name() ) ) get().add( option.name(), option );
-      else get().set( option.name(), option );
+            Debug::Throw( "XmlOptions::read - special options" );
 
-    } else {
+            // retrieve Value attribute
+            QString value( element.attribute( OPTIONS::VALUE ) );
+            if( value.size() ) get().keep( value );
 
-      // old implementation, kept for backward compatibility
-      // element name is the option name
-      XmlOption option( element );
-      if( get().isSpecialOption( option.name() ) ) get().add( option.name(), option );
-      else get().set( option.name(), option );
+        } else if( element.tagName() == OPTIONS::OPTION ) {
+
+            XmlOption option( element );
+            if( get().isSpecialOption( option.name() ) ) get().add( option.name(), option );
+            else get().set( option.name(), option );
+
+        } else {
+
+            // old implementation, kept for backward compatibility
+            // element name is the option name
+            XmlOption option( element );
+            if( get().isSpecialOption( option.name() ) ) get().add( option.name(), option );
+            else get().set( option.name(), option );
+
+        }
 
     }
 
-  }
 
-
-  return true;
+    return true;
 
 }
 
@@ -137,77 +137,77 @@ bool XmlOptions::read( QString file )
 bool XmlOptions::write( QString file )
 {
 
-  Debug::Throw() << "XmlOptions::write - " << file << endl;
+    Debug::Throw() << "XmlOptions::write - " << file << endl;
 
-  // check filename is valid
-  if( file.isEmpty() ) file = XmlOptions::file();
-  if( file.isEmpty() )
-  {
-    Debug::Throw( "XmlOptions::write - file is empty.\n" );
-    return false;
-  }
+    // check filename is valid
+    if( file.isEmpty() ) file = XmlOptions::file();
+    if( file.isEmpty() )
+    {
+        Debug::Throw( "XmlOptions::write - file is empty.\n" );
+        return false;
+    }
 
-  // output file
-  QFile out( file );
-  if( !out.open( QIODevice::WriteOnly ) )
-  {
-    Debug::Throw() << "XmlOptions::write - cannot write to file " << file << endl;
-    return false;
-  }
+    // output file
+    QFile out( file );
+    if( !out.open( QIODevice::WriteOnly ) )
+    {
+        Debug::Throw() << "XmlOptions::write - cannot write to file " << file << endl;
+        return false;
+    }
 
-  // create document
-  QDomDocument document;
+    // create document
+    QDomDocument document;
 
-  // create main element
-  QDomElement top = document.appendChild( document.createElement( OPTIONS::OPTIONS ) ).toElement();
+    // create main element
+    QDomElement top = document.appendChild( document.createElement( OPTIONS::OPTIONS ) ).toElement();
 
-  // write list of special option names
-  for( Options::SpecialMap::const_iterator iter = get().specialOptions().begin(); iter != get().specialOptions().end(); iter++ )
-  {
-    // check size of options
-    if( iter->second.empty() ) continue;
+    // write list of special option names
+    for( Options::SpecialMap::const_iterator iter = get().specialOptions().begin(); iter != get().specialOptions().end(); iter++ )
+    {
+        // check size of options
+        if( iter->second.empty() ) continue;
 
-    // dump option name
-    QDomElement element = document.createElement( OPTIONS::SPECIAL_OPTION );
-    element.setAttribute( OPTIONS::VALUE, iter->first );
-    top.appendChild( element );
-  }
+        // dump option name
+        QDomElement element = document.createElement( OPTIONS::SPECIAL_OPTION );
+        element.setAttribute( OPTIONS::VALUE, iter->first );
+        top.appendChild( element );
+    }
 
-  // write options
-  for( Options::SpecialMap::const_iterator iter = get().specialOptions().begin(); iter != get().specialOptions().end(); iter++ )
-  {
-
-    Options::List option_list( iter->second );
-    for( Options::List::iterator list_iter = option_list.begin(); list_iter != option_list.end(); list_iter++ )
+    // write options
+    for( Options::SpecialMap::const_iterator iter = get().specialOptions().begin(); iter != get().specialOptions().end(); iter++ )
     {
 
-      if( !list_iter->hasFlag( Option::RECORDABLE ) )
-      { Debug::Throw(0) << "XmlOptions::write - option " << iter->first << " is not recordable" << endl; }
+        Options::List option_list( iter->second );
+        for( Options::List::iterator list_iter = option_list.begin(); list_iter != option_list.end(); list_iter++ )
+        {
 
-      if( list_iter->hasFlag( Option::RECORDABLE ) && list_iter->set() && list_iter->raw().size() )
-      {
+            if( !list_iter->hasFlag( Option::RECORDABLE ) )
+            { Debug::Throw(0) << "XmlOptions::write - option " << iter->first << " is not recordable" << endl; }
 
-        top.appendChild( XmlOption( iter->first, *list_iter ).domElement( document ) );
+            if( list_iter->hasFlag( Option::RECORDABLE ) && list_iter->set() && list_iter->raw().size() )
+            {
 
-      } else Debug::Throw(0) << "XmlOptions::write - skipping option " << iter->first << endl;
+                top.appendChild( XmlOption( iter->first, *list_iter ).domElement( document ) );
+
+            } else Debug::Throw(0) << "XmlOptions::write - skipping option " << iter->first << endl;
+
+        }
 
     }
 
-  }
+    // write standard options
+    for( Options::Map::const_iterator iter = get().options().begin(); iter != get().options().end(); iter++ )
+    {
 
-  // write standard options
-  for( Options::Map::const_iterator iter = get().options().begin(); iter != get().options().end(); iter++ )
-  {
+        if( iter->second.hasFlag( Option::RECORDABLE ) && iter->second.set() && iter->second.raw().size() )
+        { top.appendChild( XmlOption( iter->first, iter->second ).domElement( document ) ); }
 
-    if( iter->second.hasFlag( Option::RECORDABLE ) && iter->second.set() && iter->second.raw().size() )
-    { top.appendChild( XmlOption( iter->first, iter->second ).domElement( document ) ); }
+    }
 
-  }
+    out.write( document.toByteArray() );
+    out.close();
 
-  out.write( document.toByteArray() );
-  out.close();
-
-  Debug::Throw( "XmlOptions::write - done.\n" );
-  return true;
+    Debug::Throw( "XmlOptions::write - done.\n" );
+    return true;
 
 }
--- XmlOptions.h
+++ XmlOptions.h
@@ -4,33 +4,33 @@
 // $Id$
 
 /******************************************************************************
- *
- * Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
- *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA  02111-1307 USA
- *
- *
- *******************************************************************************/
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple
+* Place, Suite 330, Boston, MA  02111-1307 USA
+*
+*
+*******************************************************************************/
 
 
 /*!
-  \file XmlOptions.h
-  \brief Option file parser based on xml
-  \author Hugo Pereira
-  \version $Revision$
-  \date $Date$
+\file XmlOptions.h
+\brief Option file parser based on xml
+\author Hugo Pereira
+\version $Revision$
+\date $Date$
 */
 
 #include "Options.h"
@@ -41,22 +41,22 @@ class XmlError;
 class XmlOptions
 {
 
-  public:
+    public:
 
-  //! retrieve singleton
-  static Options& get( void );
+    //! retrieve singleton
+    static Options& get( void );
 
-  //! read from file
-  static bool read( QString file = QString() );
+    //! read from file
+    static bool read( QString file = QString() );
 
-  //! write options to file
-  static bool write( QString file = QString() );
+    //! write options to file
+    static bool write( QString file = QString() );
 
-  //! file
-  static QString& file();
+    //! file
+    static QString& file();
 
-  //! xml error
-  static XmlError& error();
+    //! xml error
+    static XmlError& error();
 
 };
 
