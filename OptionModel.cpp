014b3f8c34a158538d45e31b477a7fe747dbdb11
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2012-02-21 17:04:33 +0000

    Fixed indentation added streamer.

--- OptionModel.cpp
+++ OptionModel.cpp
@@ -23,11 +23,11 @@
 *******************************************************************************/
 
 /*!
-  \file OptionModel.cpp
-  \brief model for object IconCaches
-  \author Hugo Pereira
-  \version $Revision$
-  \date $Date$
+\file OptionModel.cpp
+\brief model for object IconCaches
+\author Hugo Pereira
+\version $Revision$
+\date $Date$
 */
 
 #include <QPalette>
@@ -38,47 +38,47 @@
 #include "XmlOptions.h"
 
 //_______________________________________________
-const QString OptionModel::column_titles_[ OptionModel::n_columns ] =
+const QString OptionModel::columnTitles_[ OptionModel::nColumns ] =
 { "Name", "", "Current Value", "Default Value", "Flags" };
 
 //_______________________________________________
 Qt::ItemFlags OptionModel::flags(const QModelIndex &index) const
 {
-  if (!index.isValid()) return 0;
-  else if( !get( index ).second.hasFlag( Option::RECORDABLE ) ) return Qt::ItemIsSelectable;
-  else if( !( isReadOnly() || rowCount( index ) ) && index.column() == VALUE ) return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEditable;
-  else return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+    if (!index.isValid()) return 0;
+    else if( !get( index ).second.hasFlag( Option::RECORDABLE ) ) return Qt::ItemIsSelectable;
+    else if( !( isReadOnly() || rowCount( index ) ) && index.column() == VALUE ) return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEditable;
+    else return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 }
 
 //__________________________________________________________________
 QVariant OptionModel::data( const QModelIndex& index, int role ) const
 {
 
-  // check index, role and column
-  if( !index.isValid() ) return QVariant();
+    // check index, role and column
+    if( !index.isValid() ) return QVariant();
 
-  const OptionPair& option( get( index ) );
+    const OptionPair& option( get( index ) );
 
-  // return text associated to file and column
-  if( role == Qt::DisplayRole )
-  {
-    switch( index.column() )
+    // return text associated to file and column
+    if( role == Qt::DisplayRole )
     {
-      case NAME: return option.first;
-      case VALUE: return option.second.raw();
-      case DEFAULT_VALUE: return option.second.defaultValue();
-      case FLAGS: return option.second.flags();
-      default: return QVariant();
+        switch( index.column() )
+        {
+            case NAME: return option.first;
+            case VALUE: return option.second.raw();
+            case DEFAULT_VALUE: return option.second.defaultValue();
+            case FLAGS: return option.second.flags();
+            default: return QVariant();
+        }
     }
-  }
 
-  if( role == Qt::DecorationRole && index.column() == CURRENT )
-  { return option.second.isCurrent() ? IconEngine::get( ICONS::DIALOG_ACCEPT ):QVariant(); }
+    if( role == Qt::DecorationRole && index.column() == CURRENT )
+    { return option.second.isCurrent() ? IconEngine::get( ICONS::DIALOG_ACCEPT ):QVariant(); }
 
-  if( role == Qt::ToolTipRole && index.column() == NAME )
-  { return option.second.comments(); }
+    if( role == Qt::ToolTipRole && index.column() == NAME )
+    { return option.second.comments(); }
 
-  return QVariant();
+    return QVariant();
 
 }
 
@@ -86,27 +86,27 @@ QVariant OptionModel::data( const QModelIndex& index, int role ) const
 bool OptionModel::setData(const QModelIndex &index, const QVariant& value, int role )
 {
 
-  if( !(index.isValid() && index.column() == VALUE && role == Qt::EditRole ) ) return false;
+    if( !(index.isValid() && index.column() == VALUE && role == Qt::EditRole ) ) return false;
 
-  // retrieve option
-  OptionPair option( get( index ) );
+    // retrieve option
+    OptionPair option( get( index ) );
 
-  if( !( value.toString().isNull() || value.toString() == option.second.raw() ) )
-  {
+    if( !( value.toString().isNull() || value.toString() == option.second.raw() ) )
+    {
 
-    // remove old value
-    remove( option );
+        // remove old value
+        remove( option );
 
-    // add new one
-    option.second.setRaw( value.toString() );
-    add( option );
+        // add new one
+        option.second.setRaw( value.toString() );
+        add( option );
 
-    if( XmlOptions::get().isSpecialOption( option.first ) ) emit specialOptionModified( option );
-    else emit optionModified( option );
+        if( XmlOptions::get().isSpecialOption( option.first ) ) emit specialOptionModified( option );
+        else emit optionModified( option );
 
-  }
+    }
 
-  return true;
+    return true;
 
 }
 
@@ -114,15 +114,15 @@ bool OptionModel::setData(const QModelIndex &index, const QVariant& value, int r
 QVariant OptionModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
 
-  if(
-    orientation == Qt::Horizontal &&
-    role == Qt::DisplayRole &&
-    section >= 0 &&
-    section < n_columns )
-  { return column_titles_[section]; }
+    if(
+        orientation == Qt::Horizontal &&
+        role == Qt::DisplayRole &&
+        section >= 0 &&
+        section < nColumns )
+    { return columnTitles_[section]; }
 
-  // return empty
-  return QVariant();
+    // return empty
+    return QVariant();
 
 }
 
@@ -130,22 +130,22 @@ QVariant OptionModel::headerData(int section, Qt::Orientation orientation, int r
 bool OptionModel::SortFTor::operator () ( OptionPair first, OptionPair second ) const
 {
 
-  if( order_ == Qt::AscendingOrder ) std::swap( first, second );
+    if( order_ == Qt::AscendingOrder ) std::swap( first, second );
 
-  switch( type_ )
-  {
+    switch( type_ )
+    {
 
-    case NAME:
-    {
-      if( first.first != second.first ) return first.first < second.first;
-      else return first.second.raw() < second.second.raw();
+        case NAME:
+        {
+            if( first.first != second.first ) return first.first < second.first;
+            else return first.second.raw() < second.second.raw();
+        }
+        case VALUE:
+        {
+            if( first.first != second.first ) return first.second.raw() < second.second.raw();
+            else return false;
+        }
+        default: return true;
     }
-    case VALUE:
-    {
-      if( first.first != second.first ) return first.second.raw() < second.second.raw();
-      else return false;
-    }
-    default: return true;
-  }
 
 }
--- OptionModel.h
+++ OptionModel.h
@@ -25,11 +25,11 @@
 *******************************************************************************/
 
 /*!
-  \file OptionModel.h
-  \brief model for options
-  \author Hugo Pereira
-  \version $Revision$
-  \date $Date$
+\file OptionModel.h
+\brief model for options
+\author Hugo Pereira
+\version $Revision$
+\date $Date$
 */
 
 #include <string.h>
@@ -43,25 +43,32 @@
 class OptionPair: public Options::Pair
 {
 
-  public:
+    public:
 
-  //! constructor
-  OptionPair( void )
-  {}
+    //! constructor
+    OptionPair( void )
+    {}
 
-  //! constructor
-  OptionPair( const Options::Pair& option ):
-    Options::Pair( option )
-  {}
+    //! constructor
+    OptionPair( const Options::Pair& option ):
+        Options::Pair( option )
+     {}
 
-  //! constructor
-  OptionPair( const QString& name, const Option& option ):
-    Options::Pair( name, option )
-  {}
+    //! constructor
+    OptionPair( const QString& name, const Option& option ):
+        Options::Pair( name, option )
+    {}
 
-  //! returns true if this record is a child of argument
-  bool isChild( const OptionPair& option ) const
-  { return option.first == first && option.second.raw().isEmpty(); }
+    //! returns true if this record is a child of argument
+    bool isChild( const OptionPair& option ) const
+    { return option.first == first && option.second.raw().isEmpty(); }
+
+    //! write to stream
+    friend QTextStream& operator << (QTextStream& out, const OptionPair& pair )
+    {
+        out << pair.first << " " << pair.second.raw();
+        return out;
+    }
 
 };
 
@@ -69,99 +76,100 @@ class OptionPair: public Options::Pair
 class OptionModel: public TreeModel<OptionPair>, public Counter
 {
 
-  Q_OBJECT
-
-  public:
-
-  //! number of columns
-  enum { n_columns = 5 };
-
-  //! column type enumeration
-  enum ColumnType {
-    NAME,
-    CURRENT,
-    VALUE,
-    DEFAULT_VALUE,
-    FLAGS
-  };
-
-  //! constructor
-  OptionModel( QObject* parent = 0 ):
-    TreeModel<OptionPair>( parent ),
-    Counter( "OptionModel" ),
-    read_only_( true )
-  {}
-
-  //! set model read only
-  void setReadOnly( const bool& value )
-  { read_only_ = value; }
-
-  //! is read only
-  const bool& isReadOnly( void ) const
-  { return read_only_; }
-
-  //!@name methods reimplemented from base class
-  //@{
-
-  //! flags
-  virtual Qt::ItemFlags flags(const QModelIndex &index) const;
-
-  // return data for a given index
-  virtual QVariant data(const QModelIndex &index, int role) const;
-
-  // modify data
-  virtual bool setData(const QModelIndex &index, const QVariant& value, int role = Qt::EditRole );
-
-  //! header data
-  virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-
-  //! number of columns for a given index
-  virtual int columnCount(const QModelIndex &parent = QModelIndex()) const
-  { return n_columns; }
-
-  //@}
-
-  signals:
-
-  //! emmited when one special option is modified
-  void specialOptionModified( OptionPair );
-
-  //! emmited when one option is modified
-  void optionModified( OptionPair );
-
-  protected:
-
-  //! sorting
-  virtual void _sort( int column, Qt::SortOrder order = Qt::AscendingOrder )
-  { _root().sort( SortFTor( column, order ) ); }
-
-  private:
-
-  //! used to sort IconCaches
-  class SortFTor: public ItemModel::SortFTor
-  {
+    Q_OBJECT
 
     public:
 
+    //! number of columns
+    enum { nColumns = 5 };
+
+    //! column type enumeration
+    enum ColumnType
+    {
+        NAME,
+        CURRENT,
+        VALUE,
+        DEFAULT_VALUE,
+        FLAGS
+    };
+
     //! constructor
-    SortFTor( const int& type, Qt::SortOrder order = Qt::AscendingOrder ):
-      ItemModel::SortFTor( type, order )
-      {}
+    OptionModel( QObject* parent = 0 ):
+        TreeModel<OptionPair>( parent ),
+        Counter( "OptionModel" ),
+        readOnly_( true )
+    {}
 
-    //! prediction
-    bool operator() ( const Item& first, const Item& second ) const
-    { return (*this)( first.get(), second.get() ); }
+    //! set model read only
+    void setReadOnly( const bool& value )
+    { readOnly_ = value; }
 
-    //! prediction
-    bool operator() ( OptionPair, OptionPair ) const;
+    //! is read only
+    const bool& isReadOnly( void ) const
+    { return readOnly_; }
 
-  };
+    //!@name methods reimplemented from base class
+    //@{
 
-  //! true if read only
-  bool read_only_;
+    //! flags
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
 
-  //! column titles
-  static const QString column_titles_[ n_columns ];
+    // return data for a given index
+    virtual QVariant data(const QModelIndex &index, int role) const;
+
+    // modify data
+    virtual bool setData(const QModelIndex &index, const QVariant& value, int role = Qt::EditRole );
+
+    //! header data
+    virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
+    //! number of columns for a given index
+    virtual int columnCount(const QModelIndex &parent = QModelIndex()) const
+    { return nColumns; }
+
+    //@}
+
+    signals:
+
+    //! emmited when one special option is modified
+    void specialOptionModified( OptionPair );
+
+    //! emmited when one option is modified
+    void optionModified( OptionPair );
+
+    protected:
+
+    //! sorting
+    virtual void _sort( int column, Qt::SortOrder order = Qt::AscendingOrder )
+    { _root().sort( SortFTor( column, order ) ); }
+
+    private:
+
+    //! used to sort IconCaches
+    class SortFTor: public ItemModel::SortFTor
+    {
+
+        public:
+
+        //! constructor
+        SortFTor( const int& type, Qt::SortOrder order = Qt::AscendingOrder ):
+            ItemModel::SortFTor( type, order )
+        {}
+
+        //! prediction
+        bool operator() ( const Item& first, const Item& second ) const
+        { return (*this)( first.get(), second.get() ); }
+
+        //! prediction
+        bool operator() ( OptionPair, OptionPair ) const;
+
+    };
+
+    //! true if read only
+    bool readOnly_;
+
+    //! column titles
+    static const QString columnTitles_[ nColumns ];
 
 };
 
