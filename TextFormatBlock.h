f69ab9aaf66fc0cbdd72d5c84901e2e00043d3c1
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2012-05-16 12:06:45 +0000

    added support for saving hyper links inside elogbook entry editor.

--- TextFormatBlock.h
+++ TextFormatBlock.h
@@ -50,8 +50,6 @@ namespace FORMAT
             Counter( "TextFormatBlock" ),
             begin_( begin ),
             end_( end ),
-            parBegin_(0),
-            parEnd_(0),
             format_( format ),
             color_( color )
         {}
@@ -59,6 +57,9 @@ namespace FORMAT
         //! less than operator (based on Begin position)
         bool operator < (const TextFormatBlock& format ) const
         { return begin() < format.begin(); }
+
+        //!@name accessors
+        //@{
 
         //! true if format info is empty
         bool isEmpty( void ) const
@@ -68,45 +69,48 @@ namespace FORMAT
         const int& begin() const
         { return begin_; }
 
-        //! begin position
-        int& begin()
-        { return begin_; }
-
         //! end position
         const int& end() const
         { return end_; }
 
-        //! end position
-        int& end()
-        { return end_; }
-
-        //!@name obsolete paragraph indexes
-        //@{
-
-        const int& parBegin() const
-        { return parBegin_; }
-
-        //!@name obsolete paragraph indexes
-        const int& parEnd() const
-        { return parEnd_; }
-
-        //@}
-
         //! format
         const unsigned int& format() const
-        { return format_; }
-
-        //! format
-        unsigned int& format()
         { return format_; }
 
         //! color
         const QString& color() const
         { return color_; }
 
+        //! href
+        const QString& href() const
+        { return href_; }
+
+        //@}
+
+        //!@name modifiers
+        //@{
+
+        //! begin position
+        void setBegin( int value )
+        { begin_ = value; }
+
+        //! end position
+        void setEnd( int value )
+        { end_ = value; }
+
+        //! format
+        void setFormat( unsigned int value )
+        { format_ = value; }
+
         //! color
-        QString& color()
-        { return color_; }
+        void setColor( const QString& value )
+        { color_ = value; }
+
+        //! href
+        void setHRef( const QString& value )
+        { href_ = value; }
+
+        //@}
 
         //! get TextFormatBlock matching a given begin position
         class SamePositionBeginFTor
@@ -178,20 +182,6 @@ namespace FORMAT
         //! format list
         typedef QVector<FORMAT::TextFormatBlock> List;
 
-        protected:
-
-        //!@name obsolete paragraph indexes
-        //@{
-
-        int& _parBegin()
-        { return parBegin_; }
-
-        //!@name obsolete paragraph indexes
-        int& _parEnd()
-        { return parEnd_; }
-
-        //@}
-
         private:
 
         //! starting range
@@ -200,22 +190,14 @@ namespace FORMAT
         //! ending range
         int end_;
 
-        //!@name obsolete paragraph indexes
-        //@{
-
-        //! begin paragraph
-        int parBegin_;
-
-        //! end paragraph
-        int parEnd_;
-
-        //@}
-
         //! format (a bitwise or of the format bits above)
         unsigned int format_;
 
         //! color
         QString color_;
+
+        //! href
+        QString href_;
 
         //! streamer
         friend QTextStream& operator << ( QTextStream& out, const TextFormatBlock& format )
@@ -224,8 +206,10 @@ namespace FORMAT
                 << "begin: " << format.begin()
                 << " end: " << format.end()
                 << " format: " << format.format()
-                << " color: " << format.color()
-                << endl;
+                << " color: " << format.color();
+
+            if( !format.href().isEmpty() ) out << " href: " << format.href();
+            out << endl;
             return out;
         }
 
