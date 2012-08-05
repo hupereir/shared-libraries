d9e2cbe8cbd4d3d3380d70277e67be5d77f85652
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2012-08-05 15:22:43 +0000

    improved dialogs

--- QuestionDialog.cpp
+++ QuestionDialog.cpp
@@ -32,30 +32,29 @@
 
 //____________________________________________________________
 QuestionDialog::QuestionDialog( QWidget* parent, QString text ):
-CustomDialog( parent, OkButton | CancelButton| Separator )
+    CustomDialog( parent, OkButton | CancelButton| Separator )
 {
 
     Debug::Throw( "QuestionDialog::QuestionDialog\n" );
 
     //! try load Question icon
-    QPixmap question_pixmap = PixmapEngine::get( ICONS::WARNING );
-
-    if( question_pixmap.isNull() )
+    QPixmap questionPixmap = PixmapEngine::get( ICONS::WARNING );
+    if( questionPixmap.isNull() )
     {
 
         mainLayout().addWidget( label_ = new QLabel( text, this ) );
 
     } else {
 
-        QHBoxLayout *h_layout( new QHBoxLayout() );
-        h_layout->setSpacing(10);
-        h_layout->setMargin(0);
-        mainLayout().addLayout( h_layout );
+        QHBoxLayout *hLayout( new QHBoxLayout() );
+        hLayout->setSpacing(10);
+        hLayout->setMargin(0);
+        mainLayout().addLayout( hLayout );
 
         QLabel* label = new QLabel( this );
-        label->setPixmap( question_pixmap );
-        h_layout->addWidget( label, 0, Qt::AlignHCenter );
-        h_layout->addWidget( label_ = new QLabel( text, this ), 1, Qt::AlignVCenter );
+        label->setPixmap( questionPixmap );
+        hLayout->addWidget( label, 0, Qt::AlignHCenter );
+        hLayout->addWidget( label_ = new QLabel( text, this ), 1, Qt::AlignVCenter );
 
     }
 
