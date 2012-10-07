4201837697ee4f2db1e44342e2a2999de5c9fcb8
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2012-10-07 12:57:37 +0000

    Disable build timestamp on windows outside of release mode.

--- Macros.cmake
+++ Macros.cmake
@@ -1,24 +1,18 @@
 # $Id$
 MACRO( GET_BUILD_TIMESTAMP RESULT )
 
-  # find date
-  FIND_PROGRAM(DATE_PROGRAM date)
-  IF( DATE_PROGRAM )
+  IF( WIN32 AND ${RELEASE} )
 
-    IF( WIN32 )
+    EXECUTE_PROCESS( COMMAND "cmd" " /C ${DATE_PROGRAM} /T" OUTPUT_VARIABLE DATE )
+    STRING( REGEX REPLACE "(..)[/.](..)[/.](....).*" "\\3/\\2/\\1" DATE ${DATE} )
+    SET( ${RESULT} "${DATE}" )
+    MESSAGE( "-- Build timeStamp: ${${RESULT}}" )
 
-      EXECUTE_PROCESS( COMMAND "cmd" " /C ${DATE_PROGRAM} /T" OUTPUT_VARIABLE DATE )
-      STRING( REGEX REPLACE "(..)[/.](..)[/.](....).*" "\\3/\\2/\\1" DATE ${DATE} )
-      SET( ${RESULT} "${DATE}" )
-      MESSAGE( "-- Build timeStamp: ${${RESULT}}" )
+  ELSEIF( UNIX )
 
-    ELSEIF(UNIX)
-
-      EXECUTE_PROCESS( COMMAND "${DATE_PROGRAM}" "+%Y/%m/%d" OUTPUT_VARIABLE DATE )
-      STRING( REGEX REPLACE "\n+$" "" ${RESULT} ${DATE} )
-      MESSAGE( "-- Build timeStamp: ${${RESULT}}" )
-
-    ENDIF()
+    EXECUTE_PROCESS( COMMAND "date" "+%Y/%m/%d" OUTPUT_VARIABLE DATE )
+    STRING( REGEX REPLACE "\n+$" "" ${RESULT} ${DATE} )
+    MESSAGE( "-- Build timeStamp: ${${RESULT}}" )
 
   ENDIF()
 
