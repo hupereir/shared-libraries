dc1bb3ec14dc8bf0666466c9201217e187117626
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2012-10-07 13:02:38 +0000

    roll back timestamp definition.

--- Macros.cmake
+++ Macros.cmake
@@ -1,18 +1,16 @@
 # $Id$
 MACRO( GET_BUILD_TIMESTAMP RESULT )
 
-  IF( WIN32 AND ${RELEASE} )
+  IF( WIN32 )
 
-    EXECUTE_PROCESS( COMMAND "cmd" " /C ${DATE_PROGRAM} /T" OUTPUT_VARIABLE DATE )
+    EXECUTE_PROCESS( COMMAND "cmd" " /C date /T" OUTPUT_VARIABLE DATE )
     STRING( REGEX REPLACE "(..)[/.](..)[/.](....).*" "\\3/\\2/\\1" DATE ${DATE} )
     SET( ${RESULT} "${DATE}" )
-    MESSAGE( "-- Build timeStamp: ${${RESULT}}" )
 
   ELSEIF( UNIX )
 
     EXECUTE_PROCESS( COMMAND "date" "+%Y/%m/%d" OUTPUT_VARIABLE DATE )
     STRING( REGEX REPLACE "\n+$" "" ${RESULT} ${DATE} )
-    MESSAGE( "-- Build timeStamp: ${${RESULT}}" )
 
   ENDIF()
 
