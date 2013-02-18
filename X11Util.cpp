efd4e67393029694634ee293f43468200b723d4a
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2013-02-18 21:01:54 +0000

    nothing

--- X11Util.cpp
+++ X11Util.cpp
@@ -28,7 +28,8 @@
 
 #if QT_VERSION >= 0x050000
 #include <QGuiApplication>
-#include <5.0.0/QtGui/qpa/qplatformnativeinterface.h>
+// #include <5.0.0/QtGui/qpa/qplatformnativeinterface.h>
+#include <5.0.1/QtGui/qpa/qplatformnativeinterface.h>
 #else
 #include <QX11Info>
 #endif
