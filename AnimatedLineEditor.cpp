43eda1b70d761ef18d612fac6e568f8e6d8e89c8
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2013-03-21 17:33:20 +0000

    check singleton

--- AnimatedLineEditor.cpp
+++ AnimatedLineEditor.cpp
@@ -31,18 +31,20 @@
 
 //________________________________________________________
 AnimatedLineEditor::AnimatedLineEditor( QWidget* parent ):
-  LineEditor( parent ),
-  transition_widget_( new TransitionWidget(this) )
+LineEditor( parent ),
+transition_widget_( new TransitionWidget(this) )
 {
-  Debug::Throw( "AnimatedLineEditor::AnimatedLineEditor.\n" );
-  _transitionWidget().setFlag( TransitionWidget::FROM_PARENT, false );
-  _transitionWidget().hide();
+    Debug::Throw( "AnimatedLineEditor::AnimatedLineEditor.\n" );
+    _transitionWidget().setFlag( TransitionWidget::FROM_PARENT, false );
+    _transitionWidget().hide();
 
-  connect( &_transitionWidget().timeLine(), SIGNAL( finished() ),  &_transitionWidget(), SLOT( hide() ) );
-  connect( &timeLine(), SIGNAL( frameChanged( int ) ), this, SLOT( update( void )) );
-  connect( Singleton::get().application(), SIGNAL( configurationChanged( void ) ), SLOT( _updateConfiguration( void ) ) );
+    connect( &_transitionWidget().timeLine(), SIGNAL( finished() ),  &_transitionWidget(), SLOT( hide() ) );
+    connect( &timeLine(), SIGNAL( frameChanged( int ) ), this, SLOT( update( void )) );
 
-  _updateConfiguration();
+    if( Singleton::get().hasApplication() )
+    { connect( Singleton::get().application(), SIGNAL( configurationChanged( void ) ), SLOT( _updateConfiguration( void ) ) ); }
+
+    _updateConfiguration();
 
 }
 
@@ -54,15 +56,15 @@ AnimatedLineEditor::~AnimatedLineEditor( void )
 void AnimatedLineEditor::setText( const QString& text )
 {
 
-  // check enability
-  if( !( _transitionWidget().isEnabled() && isVisible() ) ) return LineEditor::setText( text );
-  else {
+    // check enability
+    if( !( _transitionWidget().isEnabled() && isVisible() ) ) return LineEditor::setText( text );
+    else {
 
-    _transitionWidget().initialize();
-    LineEditor::setText( text );
-    _transitionWidget().start();
+        _transitionWidget().initialize();
+        LineEditor::setText( text );
+        _transitionWidget().start();
 
-  }
+    }
 
 }
 
@@ -70,14 +72,14 @@ void AnimatedLineEditor::setText( const QString& text )
 void AnimatedLineEditor::clear( void )
 {
 
-  // check enability
-  if( !( _transitionWidget().isEnabled() && isVisible() && timeLine().state() == QTimeLine::NotRunning ) ) return LineEditor::clear();
-  else {
+    // check enability
+    if( !( _transitionWidget().isEnabled() && isVisible() && timeLine().state() == QTimeLine::NotRunning ) ) return LineEditor::clear();
+    else {
 
-    _transitionWidget().initialize();
-    LineEditor::clear();
-    _transitionWidget().start();
-  }
+        _transitionWidget().initialize();
+        LineEditor::clear();
+        _transitionWidget().start();
+    }
 
 }
 
@@ -85,40 +87,40 @@ void AnimatedLineEditor::clear( void )
 void AnimatedLineEditor::_updateConfiguration( void )
 {
 
-  Debug::Throw( "TransitionWidget::_updateConfiguration.\n" );
-  timeLine().setDuration( XmlOptions::get().get<int>( "SMOOTH_TRANSITION_DURATION" ) );
-  timeLine().setFrameRange( 0, XmlOptions::get().get<int>( "ANIMATION_FRAMES" ) );
+    Debug::Throw( "TransitionWidget::_updateConfiguration.\n" );
+    timeLine().setDuration( XmlOptions::get().get<int>( "SMOOTH_TRANSITION_DURATION" ) );
+    timeLine().setFrameRange( 0, XmlOptions::get().get<int>( "ANIMATION_FRAMES" ) );
 
 }
 
 //________________________________________________
 bool AnimatedLineEditor::_toggleClearButton( const bool& value )
 {
-  if( !LineEditor::_toggleClearButton( value ) ) return false;
+    if( !LineEditor::_toggleClearButton( value ) ) return false;
 
-  // check various cases where timeline should not be started
-  if( _transitionWidget().timeLine().state() != QTimeLine::NotRunning ) return true;
-  if( !(isVisible() && _transitionWidget().isEnabled() ) ) return true;
-  if( timeLine().state() != QTimeLine::NotRunning ) return true;
+    // check various cases where timeline should not be started
+    if( _transitionWidget().timeLine().state() != QTimeLine::NotRunning ) return true;
+    if( !(isVisible() && _transitionWidget().isEnabled() ) ) return true;
+    if( timeLine().state() != QTimeLine::NotRunning ) return true;
 
-  // start timeline
-  timeLine().start();
+    // start timeline
+    timeLine().start();
 
-  return true;
+    return true;
 }
 
 //________________________________________________
 void AnimatedLineEditor::_paintClearButton( QPainter& painter )
 {
 
-  // if time line is not running, run normal paint
-  if( timeLine().state() != QTimeLine::Running ) return LineEditor::_paintClearButton( painter );
+    // if time line is not running, run normal paint
+    if( timeLine().state() != QTimeLine::Running ) return LineEditor::_paintClearButton( painter );
 
-  // calculate opacity
-  qreal frame = timeLine().currentFrame();
-  if( _clearButtonVisible() ) painter.setOpacity( frame/timeLine().endFrame() );
-  else  painter.setOpacity( 1-frame/timeLine().endFrame() );
-  LineEditor::_paintClearButton( painter, false );
-  painter.setOpacity( 1 );
+    // calculate opacity
+    qreal frame = timeLine().currentFrame();
+    if( _clearButtonVisible() ) painter.setOpacity( frame/timeLine().endFrame() );
+    else  painter.setOpacity( 1-frame/timeLine().endFrame() );
+    LineEditor::_paintClearButton( painter, false );
+    painter.setOpacity( 1 );
 
 }
