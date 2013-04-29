5988982915dda4cc08e1f97dd31587b7cda1c408
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2013-04-29 07:18:59 +0000

    fixed license

--- CounterDialog.h
+++ CounterDialog.h
@@ -13,14 +13,12 @@
 * version.
 *
 * This software is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY;  without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE.   See the GNU General Public License
+* Any WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 * for more details.
 *
 * You should have received a copy of the GNU General Public License along with
-* software; if not, write to the Free Software Foundation, Inc., 59 Temple
-* Place, Suite 330, Boston, MA   02111-1307 USA
-*
+* this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 *******************************************************************************/
 
