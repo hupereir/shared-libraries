f8a1909daa7a93efebd6314b87e65f9669c43fd0
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2015-01-06 10:10:38 +0000

    fill icon states and mode explicitly rather than via braces

--- CustomToolButton.cpp
+++ CustomToolButton.cpp
@@ -61,8 +61,12 @@ bool CustomToolButton::rotate( const CustomPixmap::Rotation& value )
     QIcon copy;
 
     // loop over available sizes, modes and states
-    QList<QIcon::Mode> modes = { QIcon::Normal, QIcon::Disabled, QIcon::Active, QIcon::Selected };
-    QList<QIcon::State> states = { QIcon::Off, QIcon::On };
+    QList<QIcon::Mode> modes;
+    modes << QIcon::Normal << QIcon::Disabled << QIcon::Active << QIcon::Selected;
+
+    QList<QIcon::State> states;
+    states << QIcon::Off << QIcon::On;
+
     foreach( const QSize& size, source.availableSizes() )
     foreach( const QIcon::Mode& mode, modes )
     foreach( const QIcon::State& state, states )
