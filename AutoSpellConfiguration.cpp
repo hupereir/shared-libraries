fd5faa08f9a9aed5299da52632ac612a53653d10
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2015-01-08 14:47:02 +0000

    adjust margin

--- AutoSpellConfiguration.cpp
+++ AutoSpellConfiguration.cpp
@@ -66,6 +66,7 @@ namespace SpellCheck
 
         gridLayout->addWidget( new QLabel( tr( "Highlight font format:" ), this ), 2, 0 );
         OptionFontInfo* fontinfo =  new OptionFontInfo( this, "AUTOSPELL_FONT_FORMAT" );
+        fontinfo->layout()->setMargin(0);
         gridLayout->addWidget( fontinfo );
         fontinfo->setToolTip( tr( "Font format for misspelled words" ) );
         addOptionWidget( fontinfo );
