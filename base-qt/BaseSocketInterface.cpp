ea6a530ae614f70b6825536df5036e1a2806177e
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2016-02-22 09:03:43 +0100

    added simple socket interface

---
+++ base-qt/BaseSocketInterface.cpp
@@ -0,0 +1,77 @@
+/******************************************************************************
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* Any WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*******************************************************************************/
+
+#include "BaseSocketInterface.h"
+#include "Debug.h"
+
+//_______________________________________________________
+BaseSocketInterface::BaseSocketInterface( QObject* parent, QTcpSocket* socket ):
+  QObject( parent ),
+  Counter( "BaseSocketInterface" ),
+  socket_( socket )
+{
+    Debug::Throw( "BaseSocketInterface::BaseSocketInterface.\n" );
+    Q_CHECK_PTR( socket );
+    connect( socket_, SIGNAL(readyRead()), SLOT(_read()) );
+}
+
+//_______________________________________________________
+void BaseSocketInterface::sendBuffer( qint32 type, const QByteArray& buffer )
+{
+    quint64 bufferSize( buffer.size() );
+    socket_->write( reinterpret_cast<const char*>( &type ), sizeof( qint32 ) );
+    socket_->write( reinterpret_cast<const char*>( &bufferSize ), sizeof( quint64 ) );
+    socket_->write( buffer );
+}
+
+//_______________________________________________________
+void BaseSocketInterface::_read( void )
+{
+
+    if( !socket_->bytesAvailable() ) return;
+
+    forever
+    {
+
+        // buffer type
+        if( bufferType_ < 0 )
+        {
+            if( socket_->bytesAvailable() >= int(sizeof( qint32 )) ) socket_->read( reinterpret_cast<char*>( bufferType_ ), sizeof( qint32 ) );
+            else break;
+        }
+
+        if( bufferSize_ < 0 )
+        {
+            if( socket_->bytesAvailable() >= int(sizeof( quint64 )) ) socket_->read( reinterpret_cast<char*>( bufferSize_ ), sizeof( quint64 ) );
+            else break;
+        }
+
+        if( bufferSize_ == 0 ) break;
+        if( quint64(socket_->bytesAvailable()) >= bufferSize_ )
+        {
+
+            const QByteArray array = socket_->read( bufferSize_ );
+            emit bufferReceived( bufferType_, array );
+            bufferSize_ = 0;
+            bufferType_ = -1;
+
+        } else break;
+    }
+
+}
---
+++ base-qt/BaseSocketInterface.h
@@ -0,0 +1,104 @@
+#ifndef BaseSocketInterface_h
+#define BaseSocketInterface_h
+
+/******************************************************************************
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* Any WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*******************************************************************************/
+
+#include "Counter.h"
+
+#include <QTcpSocket>
+
+class BaseSocketInterface: public QObject, public Counter
+{
+
+    Q_OBJECT
+
+    public:
+
+    //* constructor
+    BaseSocketInterface( QObject*, QTcpSocket* );
+
+    //* destructor
+    virtual ~BaseSocketInterface( void ) = default;
+
+    //* socket
+    QTcpSocket& socket( void )
+    { return *socket_; }
+
+    //* associated socket
+    virtual const QTcpSocket& socket( void ) const
+    { return *socket_; }
+
+    //* used to retrieve all readers for a given state
+    class SameStateFTor
+    {
+      public:
+
+      //* constructor
+      SameStateFTor( QAbstractSocket::SocketState state ):
+        state_( state )
+        {}
+
+      //* destructor
+      virtual ~SameStateFTor( void )
+      {}
+
+      //* predicate
+      virtual bool operator() ( const BaseSocketInterface* reader ) const
+      { return reader->socket().state() == state_; }
+
+      private:
+
+      //* prediction
+      QAbstractSocket::SocketState state_;
+
+    };
+
+    Q_SIGNALS:
+
+    //* received buffer of given type
+    void bufferReceived( qint32, QByteArray );
+
+    public Q_SLOTS:
+
+    //* write buffer of a given type
+    void sendBuffer( qint32, const QByteArray& );
+
+    protected Q_SLOTS:
+
+    //* read message from socket
+    virtual void _read( void );
+
+    private:
+
+    //* socket
+    QTcpSocket* socket_ = nullptr;
+
+    //* running type
+    qint32 bufferType_ = -1;
+
+    //* running lenght
+    quint64 bufferSize_ = 0;
+
+    //* buffer
+    QByteArray buffer_;
+
+};
+
+#endif
