99e8373fd5e510067ca2aa39bad775d3bf91ddb1
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2016-03-05 10:38:02 +0100

    Added OpenWithDialog

---
+++ base-qt/OpenWithDialog.cpp
@@ -0,0 +1,156 @@
+/******************************************************************************
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* Any WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*******************************************************************************/
+
+#include "OpenWithDialog.h"
+
+#include "Command.h"
+#include "Debug.h"
+#include "BaseIconNames.h"
+#include "IconEngine.h"
+#include "InformationDialog.h"
+#include "ElidedLabel.h"
+#include "XmlOptions.h"
+
+#include <QDesktopServices>
+#include <QLabel>
+#include <QLayout>
+#include <QUrl>
+
+//____________________________________________________________________________
+OpenWithDialog::OpenWithDialog( QWidget* parent ):
+    CustomDialog( parent, OkButton|CancelButton|Separator )
+{
+    Debug::Throw( "OpenWithDialog::OpenWithDialog.\n" );
+    okButton().setText( tr( "Open" ) );
+}
+
+//____________________________________________________________________________
+void OpenWithDialog::realizeWidget( void )
+{
+
+    // try load Question icon
+    QHBoxLayout *hLayout( new QHBoxLayout() );
+    hLayout->setSpacing(10);
+    hLayout->setMargin(0);
+    mainLayout().addLayout( hLayout );
+    {
+        // icon
+        QLabel* label = new QLabel( this );
+        QIcon icon = icon_;
+        if( icon.isNull() ) icon = IconEngine::get( IconNames::DialogWarning );
+        label->setPixmap( icon.pixmap( iconSize() ) );
+        hLayout->addWidget( label, 0 );
+
+    }
+
+    {
+        // vertical layout for question etc.
+        QVBoxLayout* vLayout = new QVBoxLayout();
+        vLayout->setMargin(0);
+        hLayout->addLayout( vLayout );
+
+        vLayout->addStretch();
+
+        if( isLink_ )
+        {
+            vLayout->addWidget( new QLabel( tr( "Open link:" ), this ) );
+            QLabel* fileLabel = new ElidedLabel( file_, this );
+
+            // change font
+            QFont font( fileLabel->font() );
+            font.setStyle( QFont::StyleItalic );
+            fileLabel->setFont( font );
+
+            vLayout->addWidget( fileLabel );
+
+        } else {
+
+            vLayout->addWidget( new QLabel( tr( "Open file \"%1\" ? " ).arg( file_.localName() ), this ) );
+
+        }
+
+        vLayout->addStretch();
+    }
+
+    {
+        // horizontal layout for combobox
+        QHBoxLayout *hLayout( new QHBoxLayout() );
+        hLayout->setMargin(0);
+        mainLayout().addLayout( hLayout );
+
+        QLabel* label;
+        hLayout->addWidget( label = new QLabel( tr( "Open With:" ), this ), 0);
+        hLayout->addWidget( comboBox_ = new OpenWithComboBox( this ), 1 );
+        label->setAlignment( Qt::AlignVCenter|Qt::AlignRight );
+        label->setBuddy( comboBox_ );
+
+    }
+
+    // setup combobox
+    if( !optionName_.isEmpty() && XmlOptions::get().isSpecialOption( optionName_ ) )
+    {
+        // retrieve applications from options
+        const auto applications( XmlOptions::get().specialOptions( optionName_ ) );
+        foreach( auto option, applications )
+        { comboBox_->addItem( File( option.raw() ) ); }
+    }
+
+    // sytem default
+    comboBox_->insertItem(0, tr( "System Default" ) );
+    comboBox_->setCurrentIndex( 0 );
+
+    // connect
+    connect( &okButton(), SIGNAL(clicked()), this, SLOT(_open()) );
+
+}
+
+//____________________________________________________________________________
+void OpenWithDialog::_open( void )
+{
+    Debug::Throw( "OpenWithDialog::_open.\n" );
+
+    if( comboBox_->currentIndex() == 0 )
+    {
+
+        if( isLink_ ) QDesktopServices::openUrl( QUrl::fromEncoded( file_.toLatin1() ) );
+        else QDesktopServices::openUrl( QUrl::fromEncoded( QString( "file://%1" ).arg( file_ ).toLatin1() ) );
+
+    } else {
+
+        // retrieve application from combobox and add as options
+        const QString command = comboBox_->command();
+        if( command.isEmpty() || !comboBox_->isItemValid() )
+        {
+            InformationDialog( this, tr( "No command specified to open the selected files. <Open> canceled." ) ).exec();
+            return;
+        }
+
+        // update options
+        if( !optionName_.isEmpty() && XmlOptions::get().isSpecialOption( optionName_ ) )
+        {
+            foreach( auto command, comboBox_->newItems() )
+            { XmlOptions::get().add( optionName_, Option( command, Option::Recordable|Option::Current ) ); }
+        }
+
+        // execute
+        ( Command( command ) << file_ ).run();
+
+    }
+
+}
---
+++ base-qt/OpenWithDialog.h
@@ -0,0 +1,100 @@
+#ifndef OpenWithDialog_h
+#define OpenWithDialog_h
+
+/******************************************************************************
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* Any WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*******************************************************************************/
+
+#include "CustomDialog.h"
+#include "File.h"
+#include "OpenWithComboBox.h"
+
+#include <QIcon>
+
+//* QDialog used to commit selected files
+class OpenWithDialog: public CustomDialog
+{
+
+    Q_OBJECT
+
+    public:
+
+    //* constructor
+    OpenWithDialog( QWidget* );
+
+    //*@name accessors
+    //@{
+
+    //* combo box
+    OpenWithComboBox& comboBox( void ) const
+    { return *comboBox_; }
+
+    //@}
+
+    //*@name modifiers
+    //@{
+
+    //* file
+    void setFile( const File& value )
+    { file_ = value; }
+
+    //* icon
+    void setIcon( const QIcon& value )
+    { icon_ = value; }
+
+    //* type
+    void setIsLink( bool value )
+    { isLink_ = value; }
+
+    //* option name
+    void setOptionName( const QString& value )
+    {
+        optionName_ = value;
+        CustomDialog::setOptionName( QString( "%1_DIALOG" ).arg( value ) );
+    }
+
+    //* realize
+    void realizeWidget( void );
+
+    //@}
+
+    protected Q_SLOTS:
+
+    //* open file
+    void _open( void );
+
+    private:
+
+    //* file
+    File file_;
+
+    //* icon
+    QIcon icon_;
+
+    //* true if link
+    bool isLink_ = false;
+
+    //* option name
+    QString optionName_;
+
+    //* combobox
+    OpenWithComboBox* comboBox_ = nullptr;
+
+};
+
+#endif
