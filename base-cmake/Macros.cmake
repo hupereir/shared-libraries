3aced8d09db4fa22aea63ca386a5ac02251cfa44
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2017-01-18 12:27:30 +0100

    Tried to force color output in ninja (does not work)

--- base-cmake/Macros.cmake
+++ base-cmake/Macros.cmake
@@ -17,6 +17,15 @@ macro(setup_compiler_flags)
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
   endif()
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -Wall -Wundef")
+
+#   if(CMAKE_GENERATOR STREQUAL "Ninja" AND
+#     ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9) OR
+#      (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5)))
+  if(CMAKE_GENERATOR STREQUAL "Ninja")
+    # Force colored warnings in Ninja's output, if the compiler has -fdiagnostics-color support.
+    # Rationale in https://github.com/ninja-build/ninja/issues/814
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
+  endif()
 endmacro()
 
 ###################### Install Win32 application #########################
