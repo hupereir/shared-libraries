e9e5d612ca50fdf01b532d1a6d9611eabd548915
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2017-11-25 19:36:25 +0100

    fixed toolbuttonstyleFromOptions

--- base-qt/CustomToolBar.cpp
+++ base-qt/CustomToolBar.cpp
@@ -144,8 +144,8 @@ void CustomToolBar::_updateConfiguration()
     if( toolButtonStyleFromOptions_ )
     {
         const int toolButtonTextPosition( XmlOptions::get().get<int>( "TOOLBUTTON_TEXT_POSITION" ) );
-        if( toolButtonTextPosition < 0 ) setToolButtonStyle(  (Qt::ToolButtonStyle) style()->styleHint( QStyle::SH_ToolButtonStyle ) );
-        else setToolButtonStyle(  (Qt::ToolButtonStyle) toolButtonTextPosition );
+        if( toolButtonTextPosition < 0 ) QToolBar::setToolButtonStyle(  (Qt::ToolButtonStyle) style()->styleHint( QStyle::SH_ToolButtonStyle ) );
+        else QToolBar::setToolButtonStyle(  (Qt::ToolButtonStyle) toolButtonTextPosition );
     }
 
     // lock
